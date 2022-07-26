a790a631e4774d784f5f40cbfbaf12dcaeabaf71
danPyk <dan.pykalo@gmail.com>
2022-07-26 16:19:32 +0200

    detection SOMETIMES work

--- lib/object_detection/object_detection_VM.dart
+++ lib/object_detection/object_detection_VM.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:logger/logger.dart';
-//import 'package:tflite/tflite.dart';
+import 'package:tflite/tflite.dart';
 
 class ObjectDetectionVM extends ChangeNotifier {
   // late CameraController cameraController;
   // CameraImage? imgCamera;
-  // bool isCamWorking = false;
+
   //
   // void initializeCamera(bool mounted) {
   //   Future<dynamic>.delayed(Duration(seconds: 20));
@@ -24,18 +24,19 @@ class ObjectDetectionVM extends ChangeNotifier {
   //   });
   // }
 
-  // Future loadModel() async {
-  //   Tflite.close();
-  //
-  //   try {
-  //     String? response;
-  //     response = await Tflite.loadModel(
-  //       model: 'lib/object_detection/data/4.2 ssd_mobilenet.tflite',
-  //       labels: 'lib/object_detection/data/4.1 ssd_mobilenet.txt',
-  //     );
-  //   } on PlatformException {
-  //     var logger = Logger();
-  //     logger.d('Unable to load model');
-  //   }
-  // }
+  Future loadModel() async {
+
+    try {
+      String? response;
+      response = await Tflite.loadModel(
+        model: 'assets/ssd_mobilenet.tflite',
+        labels: 'assets/ssd_mobilenet.txt',
+      );
+      Tflite.close();
+
+    } on PlatformException {
+      var logger = Logger();
+      logger.d('Unable to load model');
+    }
+  }
 }
--- lib/object_detection/object_detection_screen.dart
+++ lib/object_detection/object_detection_screen.dart
@@ -2,12 +2,14 @@ import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:tflite/tflite.dart';
 
 import '../main.dart';
 import 'object_detection_VM.dart';
 
 class ObjectDetectionScreen extends StatefulWidget {
-  late final  ObjectDetectionVM viewModel;
+  late final ObjectDetectionVM viewModel;
+
   ObjectDetectionScreen({Key? key}) : super(key: key);
 
   @override
@@ -15,47 +17,86 @@ class ObjectDetectionScreen extends StatefulWidget {
 }
 
 class ObjectDetectionScreenState extends State<ObjectDetectionScreen> {
-
   late CameraController cameraController;
-  CameraImage? imgCamera;
+   CameraImage? imgCamera;
   bool isCamWorking = false;
+  late double imgHeight;
+  late double imgWidth;
+  List recongnition = [];
+  List<Widget> stackWidgets = [];
 
-  List<Widget> buildStackWidgets(BuildContext context, ObjectDetectionVM viewModel) {
-    List<Widget> stackWidgets = [
-      Positioned(
-        top: 0,
-        left: 0,
-        width: MediaQuery.of(context).size.height - 100,
-        child: Container(
-          height: MediaQuery.of(context).size.height - 100,
-          child: (!cameraController.value.isInitialized)
-              ? Container()
-              : AspectRatio(aspectRatio: cameraController.value.aspectRatio),
-        ),
-      ),
-      CameraPreview(cameraController),
-    ];
+  Future runModelOnStreamFrame() async {
+    imgHeight = imgCamera!.height + 0.0;
+    imgWidth = imgCamera!.height + 0.0;
+    recongnition = (await Tflite.detectObjectOnFrame(
+      bytesList: imgCamera!.planes.map((plane) {
+        return plane.bytes;
+      }).toList(),
+      model: 'SSDMobileNet',
+      imageHeight: imgCamera!.height,
+      imageWidth: imgCamera!.width,
+      imageMean: 127.5,
+      numResultsPerClass: 1,
+      threshold: 0.4,
+    ))!;
+    isCamWorking = false;
+    setState(() {
+      imgCamera;
+    });
+  }
 
-    return stackWidgets;
+  List<Widget> displayBoxes(Size screen) {
+    if (recongnition.isEmpty) return [];
+    if ((imgHeight == 0) || (imgWidth == 0)) return [];
+    double factorX = screen.width;
+    double factorY = imgHeight;
+    Color color = Colors.brown;
+
+    return recongnition
+        .map((result) => Positioned(
+              left: (result["rect"]["x"] as double) * factorX,
+              top: (result["rect"]["y"] as double) * factorY,
+              width: (result["rect"]["w"] as double) * factorX,
+              height: (result["rect"]["h"] as double) * factorY,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                  border: Border.all(color: Colors.brown, width: 1.0),
+                ),
+                child: Text(
+                  "{$result['detectedClass'] ${(result['confidenceInClass'] * 100).toString()}",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.0,
+                    backgroundColor: color,
+                  ),
+                ),
+              ),
+            ))
+        .toList();
   }
 
   @override
   void initState() {
     super.initState();
-   // widget.viewModel.initializeCamera(mounted);
     cameraController = CameraController(cameras[0], ResolutionPreset.max);
     cameraController.initialize().then((_) {
       if (!mounted) {
         return;
       }
       setState(() {
-        cameraController.startImageStream((imageFromStream) => {
-          if (!isCamWorking)
-            {
-              isCamWorking = true,
-              imgCamera = imageFromStream,
-            }
-        },);
+        cameraController.startImageStream(
+          (imageFromStream) => {
+            if (!isCamWorking)
+              {
+                isCamWorking = true,
+                imgCamera = imageFromStream,
+                runModelOnStreamFrame(),
+              }
+          },
+        );
       });
     }).catchError((Object e) {
       if (e is CameraException) {
@@ -89,23 +130,45 @@ class ObjectDetectionScreenState extends State<ObjectDetectionScreen> {
 
   @override
   void dispose() {
-    cameraController.dispose();
     super.dispose();
+    cameraController.stopImageStream();
+    cameraController.dispose();
+    Tflite.close();
   }
 
   @override
   Widget build(BuildContext context) {
+    stackWidgets.add(
+      Positioned(
+        top: 0,
+        left: 0,
+        width: MediaQuery.of(context).size.height - 100,
+        child: Container(
+          height: MediaQuery.of(context).size.height - 100,
+          child: (!cameraController.value.isInitialized)
+              ? Container()
+              : AspectRatio(
+                  aspectRatio: cameraController.value.aspectRatio,
+                  child: CameraPreview(cameraController),
+                ),
+        ),
+      ),
+    );
+
+    if (imgCamera != null) {
+      stackWidgets.addAll(displayBoxes(MediaQuery.of(context).size));
+    }
     return SafeArea(
       child: ViewModelBuilder<ObjectDetectionVM>.reactive(
         viewModelBuilder: () => ObjectDetectionVM(),
-        //onModelReady: (viewModel) => viewModel.initializeCamera(),
+        onModelReady: (viewModel) {
+          viewModel.loadModel();
+        },
         builder: (context, viewModel, child) => Container(
-          margin: EdgeInsets.only(top: 50),
-          child:
-
-           Stack(
-                  children: buildStackWidgets(context, viewModel),
-                ),
+          margin: const EdgeInsets.only(top: 50),
+          child: Stack(
+            children: stackWidgets,
+          ),
         ),
       ),
     );
